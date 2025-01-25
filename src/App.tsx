ff3692facdd2bb08d4e596c7c657ff017c566c18
Manav Khadka <manavkhadka2004@gmail.com>
2025-01-25 22:57:35 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          Manav the great  is {count}
+          Raghaw the great  is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
